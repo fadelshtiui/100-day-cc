861206db9a16c8a8901a522e1f482bea0c78bbf6
Fadel Shtiui <34352499+fadelshtiui@users.noreply.github.com>
2019-09-26 21:19:48 -0700

    Add files via upload

---
+++ chapter-2/day-027/README.md
@@ -0,0 +1,27 @@
+# Day 027 - Using Twilio for SMS 
+
+The purpose of this day is to have fun communicating via SMS (and with the help of `twilio`!). We will utilize twilios server to handle post and get requests, to create a dad joke generator!
+### Variables and Strings
+
+
+
+### Your Task:
+
+1. After you finish reading through this guide, visit the documentation to get an understanding how to use things
+
+2. Install all dependencies:
+    ```
+    pip install bs4
+    pip install pandas
+    https://pip.pypa.io/en/stable/installing/
+    ^ if you do not have pip installed on machine
+    ```
+
+3. create 3 functions:
+* One that gets the roster of a team
+* Two that get different types of stats
+* Convert to csv file (Extra credit)
+
+We will be using https://www.sports-reference.com/ for sports stats. They allow web scraping and have very easy-to-follow web elements.
+
+Good luck and happy coding!
---
+++ chapter-2/day-027/solution/sms.py
@@ -0,0 +1,48 @@
+# Steps:
+#test
+# 1. Create an account at Twilio --> https://www.twilio.com/try-twilio
+# 2. Verify email and phone number
+# 3. Generate trial phone number --> +12068006648
+# 4. pip install twilio
+# https://www.twilio.com/docs/sms/quickstart/python
+# https://ngrok.com/download
+# https://www.twilio.com/console/phone-numbers/incoming
+from flask import Flask, Response, request
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
+import requests
+import random
+import time
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACeb6ea9b3cde04da96cc049a60d785f0b'
+auth_token = 'c66c79bfa63921f7e43addb326fd69b7'
+app = Flask(__name__)
+
+jokes = ["Did you know theres no official training for a garbage collector?", 'I burnt my Hawaiian pizza today.', 'A doctor gave a man 6 months to live', 'How do you tuna fish?', 'What does a 6 foot 5 inch butcher weigh?']
+answers = ['They just pick it up as they go', 'I shouldve cooked it on aloha temperature...', 'the man couldnt pay his bill so the doctor gave him another 6 months', 'You raise or lower the scales', 'Meat']
+
+@app.route("/")
+def test():
+    return Response("it works!"), 200
+
+@app.route("/dad", methods=['GET', 'POST'])
+def dad_joke():
+    resp = MessagingResponse()
+    i = random.randint(0, 4)
+    msg = jokes[i] + "--> " + answers[i]
+    resp.message(msg)
+    return str(resp)
+
+@app.route("/meme", methods=['GET', 'POST'])
+def meme():
+    resp = MessagingResponse()
+    img_list = ['./memes/0.jpeg', './memes/1.jpeg', './memes/2.jpeg', './memes/3.jpeg', './memes/4.jpeg', './memes/5.jpeg']
+    i = random.randint(0, 4)
+    img = img_list[i]
+    resp.message(img)
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
